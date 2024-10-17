5b0b61ddb4cb133ee20281da861ffe9f05b138a0
Simon0824 <ez2137aaa@gmail.com>
2024-10-17 12:22:57 +0200

    switch

--- papier/Program.cs
+++ papier/Program.cs
@@ -7,6 +7,39 @@
 
     static void gra()
     {
+        Random rnd = new Random();
 
+        int robot = rnd.Next(3);
+
+        Console.WriteLine("Wybierz swój wybór - kamień (k), papier(p), czy nożyce (n)");
+        string wybor = Console.ReadLine();
+
+        int wyborgracz;
+
+        switch(wybor)
+        {
+            case "k":
+                {
+                    wyborgracz = 0;
+                    break;
+                }
+
+            case "p":
+                {
+                    wyborgracz = 1;
+                    break;
+                }
+
+            case "n":
+                {
+                    wyborgracz = 2;
+                    break;
+                }
+            default:
+                {
+                    Console.WriteLine("Zły wybór");
+                    break;
+                }
+        }
     }
-}
+}                                                                                                           
